b71da81b04fecabe6bbc1ab5824514810f105ba8
tousifws <82563595+tousifws@users.noreply.github.com>
2021-04-15 17:53:03 +0530

    added missing music container import

--- app/routeConfig.js
+++ app/routeConfig.js
@@ -1,6 +1,8 @@
 import NotFound from '@containers/NotFoundPage/Loadable';
 import HomeContainer from '@containers/HomeContainer/Loadable';
+import MusicContainer from '@containers/MusicContainer/Loadable';
 import routeConstants from '@utils/routeConstants';
+
 export const routeConfig = {
   repos: {
     component: HomeContainer,
